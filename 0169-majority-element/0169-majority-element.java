8703114eacadc17d0fb3790a0925ee5907e6ece4
Psynox-lang <78786989+Psynox-lang@users.noreply.github.com>
2024-08-24 16:07:40 +0530

    Time: 4 ms (52.69%), Space: 53.2 MB (27.98%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,22 +1,7 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int counter=0;
-        int n=nums.length;
-        int size=n/2;
-        int temp=0;
-
-        for(int i=0;i<n;i++){
-            for(int j=n-1;j>=0;j--){
-                if(nums[i]==nums[j] && i==j){
-                    counter+=1;
-                    if(counter>size){
-                        temp=nums[i];
-
-                    }
-                }
-            }
-        }
-        return temp;
-        
+        Arrays.sort(nums);
+        int n = nums.length;
+        return nums[n/2];
     }
 }
